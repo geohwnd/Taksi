feb0e6304975c0fafa8b485c25443a8b93bbc5af
geohwnd <111621310+geohwnd@users.noreply.github.com>
2023-06-07 18:32:09 -0600

    07.06.2023 Rampas
    Texturizado de Terreno y rampa incluidas

--- Assets/Taxi_Car/CarController.cs
+++ Assets/Taxi_Car/CarController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     private float horizontalInput, verticalInput;
-    private float currentSteerAngle, currentbreakForce;
+    private float currentSteerAngle, currentBreakForce;
     private bool isBreaking;
     private Vector3 initialRotation = new Vector3(0f, 0f, 0f);
 
@@ -24,110 +24,53 @@ public class CarController : MonoBehaviour
     [SerializeField] private Transform frontLeftWheelTransform, frontRightWheelTransform;
     [SerializeField] private Transform rearLeftWheelTransform, rearRightWheelTransform;
 
-    [SerializeField] private float driftForce = 500f;
-    [SerializeField] private float maxDriftAngle = 45f;
-    [SerializeField] private float driftDuration = 1f;
-    [SerializeField] private float driftCooldown = 2f;
+    [SerializeField] private Collider speedBoostCollider;
+    [SerializeField] private float speedBoostForce = 1000f;
 
-    private bool isDrifting = false;
-    private float driftStartTime = 0f;
+    private bool isBoosted = false;
 
     private void FixedUpdate()
     {
-        /*GetInput();
-        HandleMotor();
-        HandleSteering();
-        UpdateWheels();*/
         GetInput();
         HandleMotor();
-
-        if (!isDrifting)
-        {
-            HandleSteering();
-        }
-        else
-        {
-            HandleDriftSteering();
-        }
-
+        HandleSteering();
         UpdateWheels();
     }
 
     private void GetInput()
     {
-        /* // Steering Input
-         horizontalInput = Input.GetAxis("Horizontal");
-
-         // Acceleration Input
-         verticalInput = Input.GetAxis("Vertical");
-
-         // Breaking Input
-         isBreaking = Input.GetKey(KeyCode.Space);*/
-        // Steering Input
         horizontalInput = Input.GetAxis("Horizontal");
-
-        // Acceleration Input
         verticalInput = Input.GetAxis("Vertical");
-
-        // Breaking Input
         isBreaking = Input.GetKey(KeyCode.Space);
-
-        // Drift Input
-        if (Input.GetKeyDown(KeyCode.Space) && Time.time - driftStartTime > driftCooldown)
-        {
-            StartDrift();
-        }
-    }
-
-    private void StartDrift()
-    {
-        isDrifting = true;
-        driftStartTime = Time.time;
     }
 
     private void HandleMotor()
     {
         frontLeftWheelCollider.motorTorque = -verticalInput * motorForce;
         frontRightWheelCollider.motorTorque = -verticalInput * motorForce;
-        currentbreakForce = isBreaking ? breakForce : 0f;
+
+        currentBreakForce = isBreaking ? breakForce : 0f;
         ApplyBreaking();
+
+        if (isBoosted)
+        {
+            ApplySpeedBoost();
+        }
     }
 
     private void ApplyBreaking()
     {
-        frontRightWheelCollider.brakeTorque = currentbreakForce;
-        frontLeftWheelCollider.brakeTorque = currentbreakForce;
-        rearLeftWheelCollider.brakeTorque = currentbreakForce;
-        rearRightWheelCollider.brakeTorque = currentbreakForce;
+        frontRightWheelCollider.brakeTorque = currentBreakForce;
+        frontLeftWheelCollider.brakeTorque = currentBreakForce;
+        rearLeftWheelCollider.brakeTorque = currentBreakForce;
+        rearRightWheelCollider.brakeTorque = currentBreakForce;
     }
 
     private void HandleSteering()
     {
-        /*currentSteerAngle = maxSteerAngle * horizontalInput;
-        frontLeftWheelCollider.steerAngle = currentSteerAngle;
-        frontRightWheelCollider.steerAngle = currentSteerAngle;*/
         currentSteerAngle = maxSteerAngle * horizontalInput;
         frontLeftWheelCollider.steerAngle = currentSteerAngle;
         frontRightWheelCollider.steerAngle = currentSteerAngle;
-
-    }
-
-    private void HandleDriftSteering()
-    {
-        if (Time.time - driftStartTime > driftDuration)
-        {
-            isDrifting = false;
-            HandleSteering();
-            return;
-        }
-
-        float driftAngle = currentSteerAngle + (horizontalInput > 0 ? maxDriftAngle : -maxDriftAngle);
-        frontLeftWheelCollider.steerAngle = driftAngle;
-        frontRightWheelCollider.steerAngle = driftAngle;
-
-        // Apply a sideways force to create the drifting effect
-        Vector3 driftForceVector = -transform.right * (horizontalInput > 0 ? 1f : -1f) * driftForce;
-        GetComponent<Rigidbody>().AddForce(driftForceVector, ForceMode.Acceleration);
     }
 
     private void UpdateWheels()
@@ -147,41 +90,40 @@ public class CarController : MonoBehaviour
         wheelTransform.position = pos;
     }
 
+    private void ApplySpeedBoost()
+    {
+        GetComponent<Rigidbody>().AddForce(-transform.forward * speedBoostForce, ForceMode.Acceleration);
+        isBoosted = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == speedBoostCollider)
+        {
+            isBoosted = true;
+        }
+    }
+
     private void Update()
     {
         CheckIfCarFlipped();
     }
 
-    /*private void ResetCarPosition()
-    {
-        // Teleport the car to the starting position
-        transform.position = startingPosition.position;
-        transform.rotation = Quaternion.Euler(initialRotation);
-    }*/
-
     private void CheckIfCarFlipped()
     {
-        // Obtener el ángulo de inclinación actual en el eje Z
         float currentTiltAngle = transform.eulerAngles.z;
 
-        // Ajustar el ángulo negativo a un valor en el rango de 0 a 360 grados
         if (currentTiltAngle > 180f)
         {
             currentTiltAngle -= 360f;
         }
 
-        // Si el ángulo de inclinación excede el valor máximo permitido (considerado volcado)
         if (Mathf.Abs(currentTiltAngle) > maxTiltAngle)
         {
-            // Teletransportar el carro al inicio con rotación (0, 0, 0)
             transform.position = startingPosition.position;
-            transform.rotation = Quaternion.identity;
-            // Puedes usar Quaternion.Euler(0f, 0f, 0f) en lugar de Quaternion.identity si prefieres la notación de ángulos de Euler
-
-            // Reiniciar la velocidad lineal y angular del carro
+            transform.rotation = Quaternion.Euler(initialRotation);
             GetComponent<Rigidbody>().velocity = Vector3.zero;
             GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
         }
     }
-
 }
